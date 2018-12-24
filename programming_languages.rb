8046e0e8998616c46a2a6c2e149a63990246716b
github username <github email address>
2018-12-24 07:46:13 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,50 @@
+require'pry'
+
+languages = {
+    :oo => {
+        :ruby => {
+            :type => "interpreted"
+        },
+        :javascript => {
+            :type => "interpreted"
+        },
+        :python => {
+            :type => "interpreted"
+        },
+        :java => {
+            :type => "compiled"
+        }
+    },
+    :functional => {
+        :clojure => {
+            :type => "compiled"
+        },
+        :erlang => {
+            :type => "compiled"
+        },
+        :scala => {
+            :type => "compiled"
+        },
+        :javascript => {
+            :type => "interpreted"
+        }
+
+    }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+    reformated = {}
+    languages.map do |style, language|
+        language.map do |name, attribute|
+            reformated[name] = attribute
+            reformated[name][:style] = []
+            if name == :javascript
+                reformated[name][:style] = [:oo, :functional]
+            else
+                reformated[name][:style] << style
+            end
+        end
+    end
+    reformated
 end
